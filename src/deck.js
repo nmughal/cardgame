10703bf13796e10229f5a930f32d53daa8fb024b
Nadia Mughal <nmughal14@gmail.com>
2017-03-14 15:45:49 -0400

    created an iteration to shuffle the card deck

--- src/deck.js
+++ src/deck.js
@@ -1,12 +1,23 @@
-let deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
-            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+let cardValues = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
 
-// deck.length() ==== 52
+module.exports = function cardDeck() {
+  let deck = [];
 
-module.exports = function buildDeck() {
-  let shuffledDeck = [];
-  let randomCard = Math.floor(Math.random() * deck.length() )// floor rounds it down to nearest integer,
-  shuffledDeck.push(deck[randomCard]);
+  for (let i=0; i<4; i++) {
+    deck = deck.concat(cardValues);
+  }
 
-  return deck;
+let temporarySpot;
+
+  for (let i=0; i<100; i++) {
+    let randomCard = Math.floor(Math.random() * 52);
+    let randomCard2 = Math.floor(Math.random() * 52);
+    temporarySpot = deck[randomCard];
+    deck[randomCard] = deck[randomCard2];
+    temporarySpot = deck[randomCard2];
+
+  }
+  return deck; //getting back the collection
 };
+
+module.exports();
